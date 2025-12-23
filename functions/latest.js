24ac6db1e9ae23acc6a036203acb078afdf234d7
KristinaPalmquist <kristina.palmquist@gmail.com>
2025-12-23 13:19:21 +0100

    Testing other url

--- functions/latest.js
+++ functions/latest.js
@@ -1,7 +1,10 @@
 export async function onRequest(context) {
   const apiKey = context.env.HOPSWORKS_API_KEY;
 
-  const url = "https://c.app.hopsworks.ai/0.1.0/feature_store";
+  const url = "https://c.app.hopsworks.ai/hopsworks-api/api/project/2172/featurestores/2121/featureviews/air_quality_complete_fv/onlinefeatures";
+
+
+//   const url = "https://c.app.hopsworks.ai/0.1.0/feature_store";
 
   const body = {
     featureStoreName: "new_featurestore",
