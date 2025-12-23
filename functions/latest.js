9cd4f2f64c854bc48c23bccc7b5cd6e8737c516a
KristinaPalmquist <kristina.palmquist@gmail.com>
2025-12-23 13:50:45 +0100

    another url test

--- functions/latest.js
+++ functions/latest.js
@@ -1,9 +1,9 @@
 export async function onRequest(context) {
   const apiKey = context.env.HOPSWORKS_API_KEY;
 
-  const url = "https://c.app.hopsworks.ai/hopsworks-api/api/project/2172/featurestores/2121/featureviews/air_quality_complete_fv/versions/1/onlinefeatures";
-
-
+//   const url = "https://c.app.hopsworks.ai/hopsworks-api/api/project/2172/featurestores/2121/featureviews/air_quality_complete_fv/versions/1/onlinefeatures";
+// const url = "https://2172.eu-west.hopsworks.ai:4406/0.1.0/feature_store";
+const url = "https://c.app.hopsworks.ai/hopsworks-api/api//p/2172/fs/2121/fv/air_quality_complete_fv/version/1#overview";
 //   const url = "https://c.app.hopsworks.ai/0.1.0/feature_store";
 
   const body = {
