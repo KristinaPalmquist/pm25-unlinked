5e5978b08df5613bba53e0f216d92842b3d446bf
Kristina Palmquist <111965530+KristinaPalmquist@users.noreply.github.com>
2026-01-30 15:26:48 +0100

    Delete not_needed_files directory

--- not_needed_files/generate-config.mjs
+++
@@ -1,9 +0,0 @@
-import fs from "fs";
-import dotenv from "dotenv";
-
-dotenv.config();
-
-fs.writeFileSync(
-  "config.local.js",
-  `export const HOPSWORKS_API_KEY = "${process.env.HOPSWORKS_API_KEY}";`
-);
